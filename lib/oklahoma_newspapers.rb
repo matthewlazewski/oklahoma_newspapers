8453c9bfd766a5e3626d44c3cb71ef51da881d7f
matthewlazewski <matthew.lazewski@gmail.com>
2020-05-25 20:47:58 -0500

    first commit

---
+++ lib/oklahoma_newspapers.rb
@@ -0,0 +1,6 @@
+require "oklahoma_newspapers/version"
+
+module OklahomaNewspapers
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/oklahoma_newspapers/version.rb
@@ -0,0 +1,3 @@
+module OklahomaNewspapers
+  VERSION = "0.1.0"
+end
